9ed22b523f8f9ac2c216b0f8b070af5e6adcb842
Dan Moneypenny <catullus1973@gmail.com>
2022-01-30 03:19:41 -0800

    remove author and books pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
-import Authors from './pages/Authors';
-import AuthorsDetail from './pages/AuthorsDetail';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
       <h1>react boilerplate</h1>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/authors" element={<Authors/>}/>
-        <Route path="/authors/:id" element={<AuthorsDetail/>}/>
         <Route path="*" element={<PageNotFound/>}/>
       </Routes>
       
