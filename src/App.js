1ef3bf181357757d0322965133341b4f67cb77d1
Dan Moneypenny <catullus1973@gmail.com>
2022-02-05 22:24:44 -0800

    add basic auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
 // pages
 import {
@@ -9,9 +10,37 @@ import {
 } from './pages';
 
 function App() {   
+
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  if(isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if(error) {
+    return <div>oops... {error.message}</div>
+  }
+
+  if(isAuthenticated) {
     return (
-      <div><h1>Dictionary App</h1></div>
-    );
+      <div>
+        <h2>Hello {user.name}</h2>
+        <p>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>logout</button>
+        </p>
+      </div>
+    )
+  } else {
+    return <button onClick={() => loginWithRedirect}>login</button>
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// authentication
+import { DOMAIN, CLIENTID } from './constants';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENTID}
+      redirectUri={window.location.origin}
+    >
       <App/>  
+    </Auth0Provider>      
   </React.StrictMode>,
   document.getElementById('root')
 )
