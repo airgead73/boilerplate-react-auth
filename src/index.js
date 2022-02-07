0edfe0f0bc40008ebe899f08beece97332a62214
Dan Moneypenny <catullus1973@gmail.com>
2022-02-07 03:34:09 -0800

    change to scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Auth0ProviderWithHistory from './auth/Auth0-provider-with-history';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 
