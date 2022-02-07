bbba9603ca7dd0e106cbb4f625018d7f455eeecd
Dan Moneypenny <catullus1973@gmail.com>
2022-02-07 05:18:14 -0800

    add fetch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,12 @@
+import useFetch from '../hooks/useFetch';
+import { URI_PRODUCTS } from '../constants';
+
 function Dashboard() {
+
+  const { data: products, error, isPending } = useFetch(URI_PRODUCTS);
+
+  console.log(products);
+
   return (
     <div>
       <h2>dashboard</h2>
